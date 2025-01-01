e007845ce8bee2c8800108e93bbe57e1638f68a8
lorriant <brearey4@gmail.com>
2025-01-01 20:59:10 +0900

    added russian

--- locale.h
+++ locale.h
@@ -8,10 +8,28 @@
 #define scissors "Scissors"
 #define paper "Paper"
 #define your_choice "What is your choice? Please enter a number\n"
-#define incorrect_number "Incorrect number"
+#define incorrect_number "Incorrect number\n"
 #define choice_is "%s's choice is %s\n"
 #define win "%s win\n"
 #define players_identical "%s and %s are identical\n"
 #define win_with_score "%s win the game with score %d\n"
+
+#endif
+
+#ifdef RU
+
+#define how_many_levels "Сколько уровней вы хотите поиграть?\n"
+#define your_name "Как вас зовут?\n"
+#define computer "Компьютер"
+#define game_begin "Игра начинается: %s vs %s\n"
+#define rock "Камень"
+#define scissors "Ножницы"
+#define paper "Бумага"
+#define your_choice "Что выбираете? Выберите номер.\n"
+#define incorrect_number "Неверный номер\n"
+#define choice_is "%s выбрал %s\n"
+#define win "%s победил\n"
+#define players_identical "%s и %s равны\n"
+#define win_with_score "%s победил в игре и его очки = %d\n"
 
 #endif
